993003e99eb657f818d76b09fb2b3ad1ba6eccf1
ZackTarantinoWoolson <15880559+ZackTarantinoWoolson@users.noreply.github.com>
2023-04-21 18:34:36 -0400

    Allowed result to be more felxible

--- nato_translate.js
+++ nato_translate.js
@@ -1,5 +1,5 @@
 const nato_arr = [
-    ["A", "Alfa"], ["B", "Bravo"], ["C", "Charlie"], ["D", "Delta"], ["E", "Echo"], ["F", "Foxtro"], ["G", "Golf"], ["H", "Hotel"], ["I", "India"], ["J", "Juliett"], ["K", "Kilo"], ["L", "Lima"], ["M", "Mike"], ["N", "Novemeber"], ["O", "Oscar"], ["P", "Papa"], ["Q", "Quebec"], ["R", "Romeo"], ["S", "Sierra"], ["T", "Tango"], ["U", "Uniform"], ["V", "Victor"], ["W", "Whiskey"], ["X", "Xray"], ["Y", "Yankee"], ["Z", "Zulu"], [".", "Stop"], [",", "Comma"], ["1", "One"], ["2", "Two"], ["3", "Three"], ["4", "Four"], ["5", "Five"], ["6", "Six"], ["7", "Seven"], ["8", "Eight"], ["9", "Nine"], ["0", "Zero"]
+    ["A", "Alfa"], ["B", "Bravo"], ["C", "Charlie"], ["D", "Delta"], ["E", "Echo"], ["F", "Foxtro"], ["G", "Golf"], ["H", "Hotel"], ["I", "India"], ["J", "Juliett"], ["K", "Kilo"], ["L", "Lima"], ["M", "Mike"], ["N", "Novemeber"], ["O", "Oscar"], ["P", "Papa"], ["Q", "Quebec"], ["R", "Romeo"], ["S", "Sierra"], ["T", "Tango"], ["U", "Uniform"], ["V", "Victor"], ["W", "Whiskey"], ["X", "Xray"], ["Y", "Yankee"], ["Z", "Zulu"], [".", "Stop"], [",", "Comma"], ["1", "One"], ["2", "Two"], ["3", "Three"], ["4", "Four"], ["5", "Five"], ["6", "Six"], ["7", "Seven"], ["8", "Eight"], ["9", "Nine"], ["0", "Zero"],[" ","(space)"]
 ]
 
 function edValueKeyPress() {
@@ -10,20 +10,25 @@ function edValueKeyPress() {
     console.log(s.length)
     var arrayOfCharacters = s.split('');
 
-    console.log(arrayOfCharacters)
+    var result =""    
     
     for (var i = 0; i < arrayOfCharacters.length; i++) {
-    
+        
         var nato_txt = isIteminArray(arrayOfCharacters[i])
+        // console.log(arrayOfCharacters[i],nato_txt)
+        
 
         if (nato_txt === undefined) {
-            lblValue.innerText = "The text box contains: " + arrayOfCharacters[i];
+            // lblValue.innerText = "The text box contains: " + arrayOfCharacters[i]+" ";
+            result= result+arrayOfCharacters[i]+"  "
         }
         else if (nato_txt !== undefined) {
-            console.log("in else")
-            lblValue.innerText = "The text box contains: " + nato_txt;
+            // lblValue.innerText = "The text box contains: " + result;
+            result= result+nato_txt+"  "
         }
     }
+
+    lblValue.innerText = result
 }
 
 function isIteminArray(item) {
