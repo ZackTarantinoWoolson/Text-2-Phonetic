e0725ca4eb3261b8d9481f392892047eb7e932ad
ZackTarantinoWoolson <15880559+ZackTarantinoWoolson@users.noreply.github.com>
2023-04-21 18:27:25 -0400

    find character in nato array

--- nato_translate.js
+++ nato_translate.js
@@ -1,42 +1,37 @@
-const toggleSwitch = document.getElementById('theme-switch-button');
-const currentTheme = localStorage.getItem('theme');
+const nato_arr = [
+    ["A", "Alfa"], ["B", "Bravo"], ["C", "Charlie"], ["D", "Delta"], ["E", "Echo"], ["F", "Foxtro"], ["G", "Golf"], ["H", "Hotel"], ["I", "India"], ["J", "Juliett"], ["K", "Kilo"], ["L", "Lima"], ["M", "Mike"], ["N", "Novemeber"], ["O", "Oscar"], ["P", "Papa"], ["Q", "Quebec"], ["R", "Romeo"], ["S", "Sierra"], ["T", "Tango"], ["U", "Uniform"], ["V", "Victor"], ["W", "Whiskey"], ["X", "Xray"], ["Y", "Yankee"], ["Z", "Zulu"], [".", "Stop"], [",", "Comma"], ["1", "One"], ["2", "Two"], ["3", "Three"], ["4", "Four"], ["5", "Five"], ["6", "Six"], ["7", "Seven"], ["8", "Eight"], ["9", "Nine"], ["0", "Zero"]
+]
 
+function edValueKeyPress() {
+    var edValue = document.getElementById("en_text");
+    var lblValue = document.getElementById("lblValue");
+    var s = edValue.value.toUpperCase();
 
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+    console.log(s.length)
+    var arrayOfCharacters = s.split('');
 
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-        document.getElementById('theme-image').src = "/assets/images/ThemeSwitcher/sunny.png"
+    console.log(arrayOfCharacters)
+    
+    for (var i = 0; i < arrayOfCharacters.length; i++) {
+    
+        var nato_txt = isIteminArray(arrayOfCharacters[i])
 
-        var elms = document.querySelectorAll("#link-image")
-        for (var i = 0; i < elms.length; i++)
-            elms[i].src = "/assets/images/Links/external-link-white.png"
+        if (nato_txt === undefined) {
+            lblValue.innerText = "The text box contains: " + arrayOfCharacters[i];
+        }
+        else if (nato_txt !== undefined) {
+            console.log("in else")
+            lblValue.innerText = "The text box contains: " + nato_txt;
+        }
     }
 }
 
-function switchTheme(e) {
-    console.log("switch");
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        document.getElementById('theme-image').src = "/assets/images/ThemeSwitcher/sunny.png"
-
-        var elms = document.querySelectorAll("#link-image")
-        for (var i = 0; i < elms.length; i++)
-            elms[i].src = "/assets/images/Links/external-link-white.png"
-
-        localStorage.setItem('theme', 'dark');
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        document.getElementById('theme-image').src = "/assets/images/ThemeSwitcher/moon.png"
-
-        var elms = document.querySelectorAll("#link-image")
-        for (var i = 0; i < elms.length; i++)
-            elms[i].src = "/assets/images/Links/external-link-black.png"
-
-        localStorage.setItem('theme', 'light');
+function isIteminArray(item) {
+    // console.log(item)
+    for (var i = 0; i < nato_arr.length; i++) {
+        if (nato_arr[i][0] == item) {
+            // console.log(nato_arr[i][1])
+            return nato_arr[i][1]
+        }
     }
 }
-
-toggleSwitch.addEventListener('click', switchTheme, false);
