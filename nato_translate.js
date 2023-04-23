5abf043bb9ab7ea3d8a707019cf8722fe5af6674
ZackTarantinoWoolson <15880559+ZackTarantinoWoolson@users.noreply.github.com>
2023-04-23 10:00:07 -0400

    Making this into a Chrome extension

--- nato_translate.js
+++ nato_translate.js
@@ -1,5 +1,5 @@
 const nato_arr = [
-    ["A", "Alfa"], ["B", "Bravo"], ["C", "Charlie"], ["D", "Delta"], ["E", "Echo"], ["F", "Foxtro"], ["G", "Golf"], ["H", "Hotel"], ["I", "India"], ["J", "Juliett"], ["K", "Kilo"], ["L", "Lima"], ["M", "Mike"], ["N", "Novemeber"], ["O", "Oscar"], ["P", "Papa"], ["Q", "Quebec"], ["R", "Romeo"], ["S", "Sierra"], ["T", "Tango"], ["U", "Uniform"], ["V", "Victor"], ["W", "Whiskey"], ["X", "Xray"], ["Y", "Yankee"], ["Z", "Zulu"], [".", "Stop"], [",", "Comma"], ["1", "One"], ["2", "Two"], ["3", "Three"], ["4", "Four"], ["5", "Five"], ["6", "Six"], ["7", "Seven"], ["8", "Eight"], ["9", "Nine"], ["0", "Zero"],[" ","(space)"]
+    ["A", "Alfa"], ["B", "Bravo"], ["C", "Charlie"], ["D", "Delta"], ["E", "Echo"], ["F", "Foxtro"], ["G", "Golf"], ["H", "Hotel"], ["I", "India"], ["J", "Juliett"], ["K", "Kilo"], ["L", "Lima"], ["M", "Mike"], ["N", "November"], ["O", "Oscar"], ["P", "Papa"], ["Q", "Quebec"], ["R", "Romeo"], ["S", "Sierra"], ["T", "Tango"], ["U", "Uniform"], ["V", "Victor"], ["W", "Whiskey"], ["X", "Xray"], ["Y", "Yankee"], ["Z", "Zulu"], [".", "Stop"], [",", "Comma"], ["1", "One"], ["2", "Two"], ["3", "Three"], ["4", "Four"], ["5", "Five"], ["6", "Six"], ["7", "Seven"], ["8", "Eight"], ["9", "Nine"], ["0", "Zero"], [" ", "(space)"]
 ]
 
 function edValueKeyPress() {
@@ -9,22 +9,15 @@ function edValueKeyPress() {
 
     console.log(s.length)
     var arrayOfCharacters = s.split('');
-
-    var result =""    
-    
+    var result = ""
     for (var i = 0; i < arrayOfCharacters.length; i++) {
-        
         var nato_txt = isIteminArray(arrayOfCharacters[i])
-        // console.log(arrayOfCharacters[i],nato_txt)
-        
 
         if (nato_txt === undefined) {
-            // lblValue.innerText = "The text box contains: " + arrayOfCharacters[i]+" ";
-            result= result+arrayOfCharacters[i]+"  "
+            result = result + arrayOfCharacters[i] + "  "
         }
         else if (nato_txt !== undefined) {
-            // lblValue.innerText = "The text box contains: " + result;
-            result= result+nato_txt+"  "
+            result = result + nato_txt + "  "
         }
     }
 
@@ -32,11 +25,13 @@ function edValueKeyPress() {
 }
 
 function isIteminArray(item) {
-    // console.log(item)
     for (var i = 0; i < nato_arr.length; i++) {
         if (nato_arr[i][0] == item) {
-            // console.log(nato_arr[i][1])
             return nato_arr[i][1]
         }
     }
 }
+
+document.getElementById("en_text").onkeydown = function () { edValueKeyPress() };
+document.getElementById("en_text").onkeyup = function () { edValueKeyPress() };
+
